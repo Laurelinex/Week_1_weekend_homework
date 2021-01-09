4b090e904fd3ead0f5e914179bb8819ffbdd03fd
Laurelinex <laureline.vaucoy@gmail.com>
2021-01-09 15:37:46 +0000

    10th + 11th test passed

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,50 +1,50 @@
 # WRITE YOUR FUNCTIONS HERE
 
-# cc_pet_shop = {
-#     "pets": [
-#         {
-#             "name": "Sir Percy",
-#             "pet_type": "cat",
-#             "breed": "British Shorthair",
-#             "price": 500
-#         },
-#         {
-#             "name": "King Bagdemagus",
-#             "pet_type": "cat",
-#             "breed": "British Shorthair",
-#             "price": 500
-#         },
-#         {
-#             "name": "Sir Lancelot",
-#             "pet_type": "dog",
-#             "breed": "Pomsky",
-#             "price": 1000,
-#         },
-#         {
-#             "name": "Arthur",
-#             "pet_type": "dog",
-#             "breed": "Husky",
-#             "price": 900,
-#         },
-#         {
-#             "name": "Tristan",
-#             "pet_type": "cat",
-#             "breed": "Basset Hound",
-#             "price": 800,
-#         },
-#         {
-#             "name": "Merlin",
-#             "pet_type": "cat",
-#             "breed": "Egyptian Mau",
-#             "price": 1500,
-#         }
-#     ],
-#     "admin": {
-#         "total_cash": 1000,
-#         "pets_sold": 0,
-#     },
-#     "name": "Camelot of Pets"
-# }
+cc_pet_shop = {
+    "pets": [
+        {
+            "name": "Sir Percy",
+            "pet_type": "cat",
+            "breed": "British Shorthair",
+            "price": 500
+        },
+        {
+            "name": "King Bagdemagus",
+            "pet_type": "cat",
+            "breed": "British Shorthair",
+            "price": 500
+        },
+        {
+            "name": "Sir Lancelot",
+            "pet_type": "dog",
+            "breed": "Pomsky",
+            "price": 1000,
+        },
+        {
+            "name": "Arthur",
+            "pet_type": "dog",
+            "breed": "Husky",
+            "price": 900,
+        },
+        {
+            "name": "Tristan",
+            "pet_type": "cat",
+            "breed": "Basset Hound",
+            "price": 800,
+        },
+        {
+            "name": "Merlin",
+            "pet_type": "cat",
+            "breed": "Egyptian Mau",
+            "price": 1500,
+        }
+    ],
+    "admin": {
+        "total_cash": 1000,
+        "pets_sold": 0,
+    },
+    "name": "Camelot of Pets"
+}
 
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
@@ -96,3 +96,8 @@ def get_pets_by_breed(pet_shop, breed):
 
 # print(get_pets_by_breed(cc_pet_shop, "British Shorthair"))
 
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+    return None
